e5d4f5ee21cceb08a7888436afa74c392d0fd8c9
fahim.islam@outlook.com <fahim.islam@outlook.com>
2026-04-19 10:38:02 +0000

    Created StatusBadge and minecraft_server_screen; Edited theme.css and settings; Deleted Form1

--- client_code/minecraft_server_screen/__init__.py
+++ client_code/minecraft_server_screen/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import minecraft_server_screenTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class minecraft_server_screen(minecraft_server_screenTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
