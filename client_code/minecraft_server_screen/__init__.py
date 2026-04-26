847ddacd28ed5b653cdc2644296145813ea57699
fahim.islam@outlook.com <fahim.islam@outlook.com>
2026-04-26 14:39:33 +0000

    Edited Last_Updated_Badge, Player_Online_Status_Badge, Server_Status_Badge, Version_Badge and 2 more

--- client_code/minecraft_server_screen/__init__.py
+++ client_code/minecraft_server_screen/__init__.py
@@ -7,15 +7,15 @@ class minecraft_server_screen(minecraft_server_screenTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    # Run the test list immediately when the app opens
-    self.refresh_player_list()
+  #   # Run the test list immediately when the app opens
+  #   self.refresh_player_list()
 
-  def refresh_player_list(self):
-    self.player_container.clear()
+  # def refresh_player_list(self):
+  #   self.player_container.clear()
 
-    # Your manual test array
-    online_players = ["RampageIsAgo", "TheBigSniffer", "Deku19", "Notch", "Herobrine", "DASDa", "asdok34", "das94dfk", "Herobrine", "DASDa", "asdok34", "das94dfk"] 
+  #   # Your manual test array
+  #   online_players = ["RampageIsAgo", "TheBigSniffer", "Deku19", "Notch", "Herobrine", "DASDa", "asdok34", "das94dfk", "Herobrine", "DASDa", "asdok34", "das94dfk"] 
 
-    for name in online_players:
-      new_badge = Player_Name_Badge(player_name=name)
-      self.player_container.add_component(new_badge)
+  #   for name in online_players:
+  #     new_badge = Player_Name_Badge(player_name=name)
+  #     self.player_container.add_component(new_badge)
